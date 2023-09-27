ac809c09ffc94eaeb6932f1d6344412fdc28e8eb
heisdinesh <dinesh35382@gmail.com>
2023-09-27 11:37:01 +0530

    Made Navbar responsive

--- src/components/Landing/TopNavBar.jsx
+++ src/components/Landing/TopNavBar.jsx
@@ -1,45 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Pratyaksh from "../../assets/PratyakshLogo.png";
 import JharLogo from "../../assets/jharLogo.png";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const TopNavBar = () => {
+  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
+  const menuItems = [
+    { label: "About", path: "/" },
+    { label: "Services", path: "/" },
+    { label: "Blog", path: "/" },
+    { label: "Contact", path: "/" },
+    { label: "FAQ", path: "/" },
+  ];
+
   return (
-    <nav className="bg-white flex  justify-between  w-full py-4 px-4">
-      <div className="flex items-start justify-center">
+    <nav className="bg-white flex flex-col gap-2 md:flex-row justify-between w-full py-4 px-4 py-2 md:px-4">
+      <div className="hidden md:flex items-start justify-center">
         <img className="w-72" src={JharLogo} alt="" />
       </div>
-      <div className="flex flex-col items-center  justify-center">
-        {" "}
-        <img className="w-80" src={Pratyaksh} alt="" />
-        <div className="flex gap-12 justify-center  font-semibold">
-          <Link className="hover:text-primary" to="/">
-            About
-          </Link>
-          <Link className="hover:text-primary" to="/">
-            Services
-          </Link>
-          <Link className="hover:text-primary" to="/">
-            Blog
-          </Link>
-          <Link className="hover:text-primary" to="/">
-            Contact
-          </Link>
-          <Link className="hover:text-primary" to="/">
-            FAQ
-          </Link>
+      <div className="flex  flex-col items-start md:items-center justify-start md:justify-center">
+        <div className="flex items-center justify-between w-full md:w-80">
+          <img className="w-32 md:w-80" src={Pratyaksh} alt="" />
+          <div className="md:hidden">
+            <button className="text-xl" onClick={toggleMenu}>
+              {open ? <AiOutlineClose /> : <AiOutlineMenu />}
+            </button>
+          </div>
+        </div>
+        <div
+          className={`${
+            open ? "block" : "hidden"
+          } md:flex p-2 flex flex-col md:flex-row gap-4 md:gap-12 justify-center items-start font-semibold bg-background w-full md:bg-transparent`}
+        >
+          {menuItems.map((item, index) => (
+            <Link key={index} className="hover:text-primary" to={item.path}>
+              {item.label}
+            </Link>
+          ))}
         </div>
       </div>
-      <div className="flex items-center justify-center gap-4 ml-32 h-8">
+      <div className="flex items-start md:items-center justify-center gap-4 md:ml-32 h-8">
         <button
           onClick={() => navigate("/dashboard")}
           className="px-4 font-bold rounded-sm border border-primary text-primary py-1"
         >
           Get Started
         </button>
-        <button className="px-4 bg-primary text-white font-bold py-1 rounded-sm ">
+        <button className="px-8 bg-primary text-white font-bold py-1 rounded-sm">
           Login
         </button>
       </div>
